c722737a224124a1d6230f9d8d411b14331dc125
Henrique <henriqueanselmo1011@gmail.com>
2021-04-19 19:25:02 -0300

    Exercicio do dia 16.04

---
+++ NavMeshStationStarter/Assets/AgenteManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgenteManager : MonoBehaviour
+{
+    GameObject[] agents;
+
+    private void Start()
+    {
+        agents = GameObject.FindGameObjectsWithTag("ai");
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+
+            if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition),out hit,100))
+            {
+                  foreach(GameObject a in agents)
+                {
+                    a.GetComponent<AIControl>().agent.SetDestination(hit.point);
+                }
+
+            }
+        }
+    }
+}
