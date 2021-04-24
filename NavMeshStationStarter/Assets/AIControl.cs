21695f94d7ddc1f1401e96a57a9364090117c908
Henrique <henriqueanselmo1011@gmail.com>
2021-04-23 21:25:53 -0300

    Comentários feitos no script

--- NavMeshStationStarter/Assets/AIControl.cs
+++ NavMeshStationStarter/Assets/AIControl.cs
@@ -5,10 +5,12 @@ using UnityEngine.AI;
 
 public class AIControl : MonoBehaviour
 {
+    //criando a var do NavMeshAgent
     public NavMeshAgent agent;
 
     private void Start()
     {
+        //pegando o component NavMeshAgent
         agent = this.GetComponent<NavMeshAgent>();
     }
 }
